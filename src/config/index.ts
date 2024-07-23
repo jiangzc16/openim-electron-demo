d4a61e05acd5621414aab2f17f7d6c89cdd4d0d4
jiangzc <593453876@qq.com>
2024-07-23 09:55:17 +0800

    修改OPENIM_IP

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,10 +4,11 @@
 // export const API_URL = "https://web.rentsoft.cn/api";
 // export const USER_URL = "https://web.rentsoft.cn/chat";
 
-export const WS_URL = "ws://43.154.157.177:10001";
-export const API_URL = "http://43.154.157.177:10002";
-export const CHAT_URL = "http://43.154.157.177:10008";
+export const OPENIM_IP = "192.168.101.103";
+// export const OPENIM_IP = "66.110.45.47";
+// export const OPENIM_IP = "66.107.0.204"; //生产环境
+// export const OPENIM_IP = "66.75.0.11"; //生产环境
 
-export const getWsUrl = () => localStorage.getItem("wsUrl") || WS_URL;
-export const getApiUrl = () => localStorage.getItem("apiUrl") || API_URL;
-export const getChatUrl = () => localStorage.getItem("chatUrl") || CHAT_URL;
+export const WS_URL = `ws://${OPENIM_IP}:10001`;
+export const API_URL = `http://${OPENIM_IP}:10002`;
+export const USER_URL = `http://${OPENIM_IP}:10008`;
